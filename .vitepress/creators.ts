804d82e6588cb46e5d36d41b888f64f71f120363
XYXMichael <115456547+XYXMichael@users.noreply.github.com>
2024-07-20 23:52:12 +0800

    feat：update Home

--- .vitepress/creators.ts
+++ .vitepress/creators.ts
@@ -20,30 +20,28 @@ const getAvatarUrl = (name: string) => `https://github.com/${name}.png`
 
 export const creators: Creator[] = [
   {
-    name: '絢香猫',
+    name: 'XYX',
     avatar: '',
-    username: 'nekomeowww',
-    title: 'Nólëbase 原始创作者',
-    desc: '开发者，专注于基础设施维护，数据分析，后端、DevOps 开发',
+    username: 'XYXMichael',
+    title: '博客贡献者',
+    desc: '一个热衷前端开发的程序员，热爱开源，热爱分享，同时暂时也是个苦命的考研人。',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/nekomeowww' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/ayakaneko' },
+      { type: 'github', icon: 'github', link: 'https://github.com/XYXMichael' },
     ],
-    nameAliases: ['nekomeowww', '绚香猫', '絢香猫', 'Neko Ayaka', 'Ayaka Neko'],
-    emailAliases: ['neko@ayaka.moe'],
+    nameAliases: ['XYXMichael', 'XYX', 'XYX', 'Michael XYX', 'Michael XYX'],
+    emailAliases: ['1907214633@qq.com'],
   },
   {
-    name: '絢香音',
+    name: 'BSY',
     avatar: '',
-    username: 'LittleSound',
+    username: 'Leon-Bin',
     title: 'Nólëbase 原始创作者',
     desc: '开源开发者，专注于前端，以及前端相关工具库和工具链开发',
     links: [
-      { type: 'github', icon: 'github', link: 'https://github.com/LittleSound' },
-      { type: 'twitter', icon: 'twitter', link: 'https://twitter.com/OikawaRizumu' },
+      { type: 'github', icon: 'github', link: 'https://github.com/Leon-Bin' },
     ],
-    nameAliases: ['LittleSound', '绚香音', '絢香音', 'Rizumu Oikawa', 'Rizumu Ayaka', 'Ayaka Rizumu', 'Rizumu'],
-    emailAliases: ['rizumu@ayaka.moe', 'rizumu@oqo.moe'],
+    nameAliases: ['Leon-Bin', 'BSY', 'BSY', 'Bin Leon', 'Bin Leon'],
+    emailAliases: ['bin2946919972@gmail.com'],
   },
 ].map<Creator>((c) => {
   c.avatar = c.avatar || getAvatarUrl(c.username)
--- index.md
+++ index.md
@@ -1,14 +1,12 @@
 ---
 layout: home
 sidebar: false
-
-title: Nólëbase
+title: XYXMichiael
 titleTemplate: 记录回忆，知识和畅想的地方
-
 hero:
   name: g~Nj$3J2^
   text: 记录回忆，知识和畅想的地方
-  tagline: 以 Nólëbase 为名，读作 nole-base，取自意为「知识」的昆雅语 nólë 和意为「基础」的英文 base，即「知识库」
+  tagline: 一个考研人在不想复习的情况下，通过寻找适合自己的开源博客进行搭建，记录自己的学习历程。【寻找了两个有缘人一起创建属于自己的Blog】
   image:
     src: /logo.svg
     alt: Vitest
@@ -17,25 +15,26 @@ hero:
       text: 开始阅读
       link: /笔记/index
     - theme: alt
-      text: 加入 Discord 服务器
-      link: https://discord.gg/XuNFDcDZGj
-    - theme: alt
       text: GitHub 上浏览
       link: https://github.com/nolebase/nolebase
-
 features:
-  - title: 多样的主题和内容
-    details: 本知识库和所生成的页面均由创作者们维护，涉及到生活中各方面知识和内容，也不乏我们的回忆和畅想。
-    icon: 🌈
-  - title: 皆为 Markdown
-    details: 使用 Markdown 和 Markdown 拓展语法编写和记录笔记，每一个页面都是 Markdown 文件。
-    icon: 📃
-  - title: 由 VitePress 驱动
-    details: 基于 Vite 的强大静态文档页面生成器，它生成了我们知识库的页面，提供了简单易用的主题和工具。
-    icon: 🚀
-  - title: 由 Obsidian 驱动
-    details: 强大的知识库管理工具，支持花样繁多的插件和拓展，让知识管理变得更加简单。
-    icon: 🗃
+  - title: 数据结构
+    details: 记录408痛苦的学习历程，分享数据结构的学习心得。
+    icon: 📚
+    link:/笔记/数据结构/栈、队列和数组
+  - title: 计算机网络
+    details: 下一个需要复习的408科目，分享计算机网络的学习心得。
+    icon: 🌐
+  - title: 操作系统
+    details: 记录操作系统的学习历程，分享操作系统的学习心得。
+    icon: 🖥️
+  - title: 计算机组成原理
+    details: 记录408痛苦的学习历程，分享计算机组成原理的学习心得。
+    icon: ⚙️
+  - title: 旅游Blog
+    details: 记录旅游的点滴感受，分享旅游的心得体会。
+    icon: ✈️
+    link：/笔记/北京旅游（摆烂式旅游）
 ---
 
 <HomePage />
